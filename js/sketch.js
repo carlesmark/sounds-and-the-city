d945a8462f015a581b3c5c5bd8128b227513dfb1
Mark Carles <36423733+carlesmark@users.noreply.github.com>
2018-05-04 18:33:51 -0400

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -4,14 +4,14 @@ var presscount=1;
 function preload() {
     slide_array[1]=loadImage("img/soundchart1.jpg");
     slide_array[2]=loadImage("img/soundchart2.jpg");
-//     slide_array[3]=loadImage("assets/c.svg");
-//     slide_array[4]=loadImage("assets/d.svg");
-//     slide_array[5]=loadImage("assets/e.svg");
-//     slide_array[6]=loadImage("assets/f.svg");
-//     slide_array[7]=loadImage("assets/g.svg");
-//     slide_array[8]=loadImage("assets/h.svg");
-//     slide_array[9]=loadImage("assets/i.svg");
-//     slide_array[10]=loadImage("assets/j.svg");
+    slide_array[3]=loadImage("img/soundchart3.jpg");
+    slide_array[4]=loadImage("img/soundchart4.jpg");
+    slide_array[5]=loadImage("img/soundchart5.jpg");
+    slide_array[6]=loadImage("assets/f.svg");
+    slide_array[7]=loadImage("assets/g.svg");
+    slide_array[8]=loadImage("assets/h.svg");
+    slide_array[9]=loadImage("assets/i.svg");
+    slide_array[10]=loadImage("assets/j.svg");
     
 }
 function setup() {
@@ -23,7 +23,7 @@ function draw() {
 }
 
 function mousePressed(){
- if(presscount==10)
+ if(presscount==18)
  {
   presscount=0;
  }
