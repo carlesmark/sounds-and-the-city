e0da87f58673766d890389135df4820d62a5616d
Mark Carles <36423733+carlesmark@users.noreply.github.com>
2018-05-04 18:23:41 -0400

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,20 +1,33 @@
-var mapimg;
-
-var lat = 0;
-var lon = 0;
-
+var slide_array=[];
+var presscount=1;
 
 function preload() {
-  mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/streets-v9/static/-122.4241,37.78,14.25,0,0/1024x512?access_token=pk.eyJ1IjoibWFya2NhcmxlcyIsImEiOiJjamV5eWlxMHMwNG1rMndvOTV6YW05OG55In0.ZgvmYWzuVeTkM8AuD3EHIQ');
+    slide_array[1]=loadImage("img/soundchart1.jpg");
+    slide_array[2]=loadImage("img/soundchart2.jpg");
+    slide_array[3]=loadImage("assets/c.svg");
+    slide_array[4]=loadImage("assets/d.svg");
+    slide_array[5]=loadImage("assets/e.svg");
+    slide_array[6]=loadImage("assets/f.svg");
+    slide_array[7]=loadImage("assets/g.svg");
+    slide_array[8]=loadImage("assets/h.svg");
+    slide_array[9]=loadImage("assets/i.svg");
+    slide_array[10]=loadImage("assets/j.svg");
+    
+}
+function setup() {
+  createCanvas(displayWidth,displayHeight);
+}  
 
+function draw() {
+   image(slide_array[presscount],100,0);
 }
 
-function setup(){
-  createCanvas(1024, 512);
-  image(mapimg, 0, 0);
-
+function mousePressed(){
+ if(presscount==10)
+ {
+  presscount=0;
+ }
+presscount++;
+clear();
 }
-function draw(){
 
-
-}
