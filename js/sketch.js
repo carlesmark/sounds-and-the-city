508149bf9cab42356cf35e06a2eff4aadbd39e53
Mark Carles <36423733+carlesmark@users.noreply.github.com>
2018-05-06 10:43:19 -0400

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -12,7 +12,7 @@ function preload() {
     slide_array[8]=loadImage("img/soundchart8.jpg");
     slide_array[9]=loadImage("img/soundchart9.jpg");
     slide_array[10]=loadImage("img/soundchart10.jpg");
-//     slide_array[11]=loadImage("img/soundchart11.jpg");
+    slide_array[11]=loadImage("img/soundchart11.jpg");
 //     slide_array[12]=loadImage("img/soundchart12.jpg");
 //     slide_array[13]=loadImage("img/soundchart13.jpg");
 //     slide_array[14]=loadImage("img/soundchart14.jpg");
